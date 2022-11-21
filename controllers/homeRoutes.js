7dd544d1ce46d19edae98252e5ab00ed2b7723af
Felisha <felisha.j.yu@gmail.com>
2022-11-21 15:32:19 -0800

    added homepage and route for all blogs to show

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,17 @@ const { Blog, Comment, User } = require('../models');
 
 // GET homepage
 router.get('/', async (req, res) => {
-  res.render('home', {
-    loggedIn: req.session.loggedIn,
-    user_id: req.session.user_id
+  Blog.findAll().then(blogs=>{
+    const blogsHbsData = blogs.map(blog=>blog.get({plain:true}))
+    console.log(blogs);
+    console.log("==============")
+    console.log(blogsHbsData) 
+    
+    res.render('homepage', {
+      blogs:blogsHbsData,
+      loggedIn: req.session.loggedIn,
+      user_id: req.session.user_id
+  })
   })
 })
 
