51fbfba24d7f4b0a05f0c98d2dc0789552b894cd
Felisha <felisha.j.yu@gmail.com>
2022-11-22 10:00:59 -0800

    got new comment to post and started dashboard

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,31 @@
-const commentForm = document.querySelector("#commentForm");
-commentForm.addEventListener("submit",e=>{
+console.log("comment js linked")
+
+const commentForm=document.querySelector('#commentForm');
+commentForm.addEventListener("submit", e=> {
     e.preventDefault();
-    console.log('PREVENTED DEFAULT!')
+    console.log(`Prevented Default`)
+    const comment_text= document.querySelector('#comment_text').value
+    const blog_id=document.querySelector('.new-comment-form').dataset.blogid
+
     const commentObj = {
-        comment_text:document.querySelector("#comment_text").value,
+        blog_id:blog_id,
+        comment_text:comment_text
     }
-    fetch("/api/comments/",{
+    
+    console.log(commentObj)
+    fetch("/api/comments",{
         method:"POST",
         body:JSON.stringify(commentObj),
         headers:{
             "Content-Type":"application/json"
         }
-    }).then(res=>{
+    }).then (res=>{
         if(res.ok){
+            console.log(res)
             alert("comment added!")
             location.reload()
         } else {
             alert("error posting comment")
         }
     })
-})
+}) 
